770530707c51bdf673e0993a3f73fd2bbb922d16
mahendra575419 <vvmahendrakumar@gmail.com>
2024-11-06 21:10:15 -0600

    everything is done

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var instrumentRouter = require('./routes/instrument');
+var gridRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick');
+
 var app = express();
 
 // view engine setup
@@ -22,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/instrument', instrumentRouter);
+app.use('/grid', gridRouter);
+app.use('/pick', pickRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
